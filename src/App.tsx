e4b697eb64920d29184d27ecbcfde6dccb5355c7
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-10-12 23:16:57 -0300

    API fake

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+//@libraries
+import { createServer } from "miragejs";
+
+//@components
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+
+createServer({
+  routes() {
+    this.namespace = "/api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de Software",
+          amount: 3000,
+          type: "deposit",
+          category: "Venda",
+        },
+      ];
+    });
+  },
+});
 
 function App() {
   return (
